40eba334f1fae29ca31fd0d8600e35051ea25b2d
bbolker <bbolker@gmail.com>
2015-04-20 17:41:46 -0400

    beginnings of new vignette on grad comp

---
+++ vignettes/sirgrad.c
@@ -0,0 +1,33 @@
+/* SIR_grad_logI .c */
+/* Author : Irena Papst */
+/* SIR gradient in C for use with the fitsir package in R */
+# include <R.h> // include R library
+static double parms[3];
+# define beta parms[0]
+# define gamma parms[1]
+# define N parms[2]
+
+/* initializer */
+void initmod ( void (*odeparms)(int *, double *) )
+{
+    int num=3;
+    odeparms (&num, parms) ;
+}
+/* Derivatives and 1 output variable */
+void derivs (int *neq , double *t , double *vars , double *varsdot ,
+	     double *varsout , int *ip )
+{
+    if (ip[0] <1) error ( "nout should be at least 1" ) ;
+    varsdot[0] = - beta * vars[0]* exp (vars[1]) / N ; // equation for S
+    varsdot[1] = beta * vars[0]/ N - gamma ; // equation for log (I)
+}
+
+# define vbeta vparms[0]
+# define vgamma vparms[1]
+# define vN vparms[2]
+
+void derivs0 (double *t , double *vars , double *varsdot, double *vparms ) 
+{
+    varsdot[0] = - vbeta * vars[0]* exp (vars[1]) / vN ; // equation for S
+    varsdot[1] = vbeta * vars[0]/ vN - vgamma ; // equation for log (I)
+}
---
+++ vignettes/sirgrad.cpp
@@ -0,0 +1,32 @@
+#include <Rcpp.h>     // header files, definitions etc.
+using namespace Rcpp; // can use Rcpp functions without doing Rcpp::
+
+// we can't use with() any more, we have to define
+//  the positions of the parameters; indexing from 0
+// "compiler"/macro definitions, very fast
+#define beta pars[0]
+#define gamma pars[1]
+#define N pars[2]
+#define S y[0]
+#define logI y[1]
+#define Sdot res[0]
+#define logIdot res[1]
+
+// some stuff I looked at while I was figuring this out
+// http://gallery.rcpp.org/articles/sugar-for-high-level-vector-operations/
+// http://cran.r-project.org/web/packages/Rcpp/vignettes/Rcpp-quickref.pdf
+
+// telling compiler that we want to use this function in R
+// [[Rcpp::export]]  // magic 
+
+List sirgrad(double t, NumericVector y, NumericVector pars) {
+    int n = y.size();
+    NumericVector res( n );
+    Sdot = -beta/N*exp(logI)*S;
+    logIdot = beta/N*S-gamma;
+    List ret;   // make a new list
+    ret["grad"] = res;  // put the result into it
+    return(ret);
+
+}
+
