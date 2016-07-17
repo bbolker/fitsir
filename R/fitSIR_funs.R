46dabc2aea1593e53b3d9fa67ebeaa14fee0fa7b
Ben Bolker <bbolker@gmail.com>
2016-07-17 15:49:33 -0400

    tinkering with autostart

--- R/fitSIR_funs.R
+++ R/fitSIR_funs.R
@@ -6,82 +6,106 @@
 ##' @param logit.i logit of initial proportion infectious
 ##' @export
 startfun <- function(log.beta=log(0.12),log.gamma=log(0.09),
-         log.N=log(10000),logit.i=qlogis(0.01),auto=FALSE, incidence = FALSE,
-         data) {
-	if (auto) {
-		tvec <- data$tvec
-		count <- data$count
-		## smooth data; start with smoothing par 0.5, try
-		## to increase it until there is a single critical point ...
-		## (check that second deriv is negative???)
-		ncrit <- Inf
-		it <- 1
-		spar <- 0.5
-		while (ncrit>1 && it<10) {
-			ss <- smooth.spline(tvec,log(count),spar=spar)
-			dd <- predict(ss,deriv=1)$y
-			ncrit <- sum(diff(sign(dd))!=0)
-			spar <- (1+spar)/2
-		}
-		if (it==10) stop("couldn't smooth enough")
-		ss.data <- data.frame(tvec = tvec, count = exp(predict(ss)$y))
-		## find max value
-		ss.tmax <- uniroot(function(x) predict(ss,x,deriv=1)$y,range(tvec))$root
-		## find a point halfway between initial and max
-		##  scaling could be adjustable?
-		ss.thalf <- min(tvec)+0.5*(ss.tmax-min(tvec))
-		m1 <- lm(log(count)~tvec,data=subset(ss.data,tvec<ss.thalf))
-		r <- as.numeric(coef(m1)[2]) ##beta - gamma
-		iniI <- ss.data$count[1] ## N * i0
-	    ## curvature of spline at max
+                     log.N=log(10000),logit.i=qlogis(0.01),
+                     auto=FALSE, incidence = FALSE,
+                     data,itmax=100,relpeakcrit=0.1) {
+    if (auto) {
+        tvec <- data$tvec
+        count <- data$count
+        ## for smooth.spline(log(count)) ...
+        if (any(count<=0)) {
+            count <- pmax(count,min(count[count>0])/2)
+        }
+        ## smooth data; start with smoothing par 0.5, try
+        ## to increase it until there is a single critical point ...
+        ## (check that second deriv is negative???)
+        single_peak <- FALSE
+        it <- 1
+        spar <- 0.5
+        while (!single_peak && it<itmax) {
+            ss <- smooth.spline(tvec,log(count),spar=spar)
+            dd <- predict(ss,deriv=1)$y
+            ## change in sign of first derivative
+            dds <- diff(sign(dd))
+            spar <- if (spar<0.8) spar+0.05 else (1+spar)/2
+            it <- it+1
+            ncrit <- sum(dds>0)
+            peakvals <- count[dds>0]
+            relpeakvals <- peakvals[-1]/peakvals[1]
+            single_peak <- ncrit==1 ||
+                all(relpeakvals<relpeakcrit)
+        }
+        ss.data <- data.frame(tvec = tvec, count = exp(predict(ss)$y))
+        if (it==itmax) {
+            ## try harder?
+            stop("couldn't smooth enough")
+        }
+        ## find max value:
+        ##  finding max based on data is a bit more robust
+        ##   hard to distinguish between max and min with predict()
+        ##    ufun <- function(x) predict(ss,x,deriv=1)$y
+        ##    ss.tmax <- uniroot(ufun,range(tvec))$root
+        ##
+        ## ??? what do we do if smoothing until we have only a single
+        ##   critical point 
+        ss.tmax <- ss.data$tvec[which.max(ss.data$count)]
+        ## find a point halfway between initial and max
+        ##  scaling could be adjustable?
+        ss.thalf <- min(tvec)+0.5*(ss.tmax-min(tvec))
+        m1 <- lm(log(count)~tvec,data=subset(ss.data,tvec<ss.thalf))
+        r <- as.numeric(coef(m1)[2]) ##beta - gamma
+        iniI <- ss.data$count[1] ## N * i0
+        ## curvature of spline at max
+        
+        if (incidence) {
+            N = cumsum(count)[length(tvec)]
+            t.diff <- diff(tvec)
+            t.diff <- c(t.diff[1], t.diff)
+            
+            P <- ss.data$count/t.diff
+
+            ## DRY ...
+            ncrit <- Inf
+            it <- 1
+            spar <- 0.5
+            while (ncrit>1 && it<10) {
+                ss <- smooth.spline(tvec,log(P),spar=spar)
+                dd <- predict(ss,deriv=1)$y
+                ncrit <- sum(diff(sign(dd))!=0)
+                spar <- (1+spar)/2
+            }
+        } ## if incidence
+        
+        Qp.alt <- predict(ss,ss.tmax,deriv=2)$y
+        Ip <- exp(max(predict(ss,tvec)$y))
+        c <- -Qp.alt/Ip
+        
+        if (incidence) {
+            gamma <- 0.5 * (sqrt(4*c*N + r^2)-r)
+            beta <- gamma + r
+            N <- N
+            d <- iniI/(t.diff[1]* beta * N)
+            i0 <- 0.5 * (1-sqrt(1-4*d))
+        } else {
+            gamma <- -Qp.alt/r
+            beta <- gamma + r
+            N <- beta*gamma/c
+            i0 <- iniI/N
+        }
+        
+        x <- list(
+            log.beta = log(beta),
+            log.gamma = log(gamma),
+            log.N = log(N),
+            logit.i = qlogis(i0)
+        )
+        
+        return(unlist(x))
+    } ## if (auto)
+    ## ... why are we returning a list in one case and a vector in the other??
     
-    if(incidence){
-      N = cumsum(count)[length(tvec)]
-      t.diff <- diff(tvec)
-      t.diff <- c(t.diff[1], t.diff)
-      
-      P <- ss.data$count/t.diff
-      
-      ncrit <- Inf
-      it <- 1
-      spar <- 0.5
-      while (ncrit>1 && it<10) {
-        ss <- smooth.spline(tvec,log(P),spar=spar)
-        dd <- predict(ss,deriv=1)$y
-        ncrit <- sum(diff(sign(dd))!=0)
-        spar <- (1+spar)/2
-      }
-    }
-    
-		Qp.alt <- predict(ss,ss.tmax,deriv=2)$y
-		Ip <- exp(max(predict(ss,tvec)$y))
-		c <- -Qp.alt/Ip
-    
-    
-    if(incidence){
-      gamma = 0.5 * (sqrt(4*c*N + r^2)-r)
-      beta = gamma + r
-      N = N
-      d = iniI/(t.diff[1]* beta * N)
-      i0 = 0.5 * (1-sqrt(1-4*d))
-    }else{
-		  gamma = -Qp.alt/r
-		  beta = gamma + r
-		  N = beta*gamma/c
-		  i0 = iniI/N
-    }
-    
-		x <- list(
-			log.beta = log(beta),
-			log.gamma = log(gamma),
-			log.N = log(N),
-			logit.i = qlogis(i0)
-		)
-		
-		return(unlist(x))
-	}
-	list(log.beta=log.beta,log.gamma=log.gamma,log.N=log.N,
-		logit.i=logit.i)
+    list(log.beta=log.beta,log.gamma=log.gamma,log.N=log.N,
+         logit.i=logit.i)
 }
 
 ##' Normal distribution with profiled sd
@@ -101,45 +125,45 @@ dnorm2 <- function(x,mean,log=FALSE) {
 ##' @return gradient vector for a simple SIR model
 ##' @export
 SIR.grad <- function(t, x, params) {
-	g <- with(as.list(c(x,params)),
-	{
-		I = exp(logI)
-		dS = -beta*exp(logI)*S/N
-		dlogI = beta*S/N-gamma
-		list(c(dS,dlogI), I = I)
-	})
+    g <- with(as.list(c(x,params)),
+    {
+        I = exp(logI)
+        dS = -beta*exp(logI)*S/N
+        dlogI = beta*S/N-gamma
+        list(c(dS,dlogI), I = I)
+    })
 }
 
 SIR.grad.sens <- function(t, x, params) {
-	g <- with(as.list(c(x,params)),
-	{
-		I = exp(logI)
-		dS = -beta*S*I/N
-		dlogI = beta*S/N-gamma
-		
-		grad_SS = - beta * I/N
-		grad_SI = - beta * S/N
-		grad_IS = beta*I/N
-		grad_II = beta*S/N-gamma
+    g <- with(as.list(c(x,params)),
+    {
+        I = exp(logI)
+        dS = -beta*S*I/N
+        dlogI = beta*S/N-gamma
+        
+        grad_SS = - beta * I/N
+        grad_SI = - beta * S/N
+        grad_IS = beta*I/N
+        grad_II = beta*S/N-gamma
 	
-		dnu_beta_S = grad_SS * nu_beta_S + grad_SI * nu_beta_I - S*I/N
+        dnu_beta_S = grad_SS * nu_beta_S + grad_SI * nu_beta_I - S*I/N
 	
-		dnu_N_S = grad_SS * nu_N_S + grad_SI * nu_N_I + beta*S*I/N^2
+        dnu_N_S = grad_SS * nu_N_S + grad_SI * nu_N_I + beta*S*I/N^2
 	
-		dnu_gamma_S = grad_SS * nu_gamma_S + grad_SI * nu_gamma_I
+        dnu_gamma_S = grad_SS * nu_gamma_S + grad_SI * nu_gamma_I
 	
-		dnu_I0_S = grad_SS * nu_I0_S + grad_SI * nu_I0_I
+        dnu_I0_S = grad_SS * nu_I0_S + grad_SI * nu_I0_I
 	
-		dnu_beta_I = grad_IS * nu_beta_S + grad_II * nu_beta_I + S*I/N
+        dnu_beta_I = grad_IS * nu_beta_S + grad_II * nu_beta_I + S*I/N
 	
-		dnu_N_I = grad_IS * nu_N_S + grad_II * nu_N_I - beta*S*I/N^2
+        dnu_N_I = grad_IS * nu_N_S + grad_II * nu_N_I - beta*S*I/N^2
 	
-		dnu_gamma_I = grad_IS * nu_gamma_S +  grad_II * nu_gamma_I - I
+        dnu_gamma_I = grad_IS * nu_gamma_S +  grad_II * nu_gamma_I - I
 	
-		dnu_I0_I = grad_IS * nu_I0_S + grad_II * nu_I0_I
+        dnu_I0_I = grad_IS * nu_I0_S + grad_II * nu_I0_I
 	
-		list(c(dS, dlogI, dnu_beta_S, dnu_gamma_S, dnu_N_S, dnu_I0_S, dnu_beta_I, dnu_gamma_I, dnu_N_I, dnu_I0_I), I = I)
-	})
+        list(c(dS, dlogI, dnu_beta_S, dnu_gamma_S, dnu_N_S, dnu_I0_S, dnu_beta_I, dnu_gamma_I, dnu_N_I, dnu_I0_I), I = I)
+    })
 }
 
 ##' additive log-ratio transformation and inverse
@@ -202,63 +226,63 @@ summarize.pars <- function(params) {
 ##' plot(tvec,ss,type="l",xlab="time",ylab="infected")
 SIR.detsim <- function(t, params, findSens = FALSE,
                        incidence = FALSE, reportAll = FALSE){
-	with(as.list(params),{
-		if(incidence){
-			l <- length(t)
-			t.d <- diff(t[(l-1):l])
-			t <- c(t, t[l]+t.d)
-			t.interval <- diff(t)
-		}
-		
-		if(findSens){
-			func <- SIR.grad.sens
-			yini <- c(S = N*(1-i0), logI = log(N*i0),
-				nu_beta_S = 0, nu_gamma_S = 0, nu_N_S = 1-i0,
-				nu_I0_S = -N,
-				nu_beta_I = 0, nu_gamma_I = 0, nu_N_I = i0,
-				nu_I0_I = N)
-		}else{
-			func <- SIR.grad
-			yini <- c(S=N*(1-i0),logI=log(N*i0))
-		}
-		
-		odesol <- as.data.frame(ode(y=yini,
-			times=t,
-			func=func,
-			parms=params,
-			dllname = "fitsir",
-			initfunc = "initmod"))
-		
-		if(findSens){
-			sensName = c("nu_beta_S", "nu_gamma_S", "nu_N_S", "nu_I0_S", "nu_beta_I", "nu_gamma_I", "nu_N_I", "nu_I0_I")
-			
-			logSens <- c(beta, gamma, N, i0^2*exp(-qlogis(i0)))
-			
-			odesol[,sensName] = sweep(odesol[,sensName], 2, rep(logSens,2), "*")
-		}
-		
-		if(reportAll){
-			return(odesol)
-		}
-		
-		if(incidence){
-			odesol <- as.data.frame(diff(as.matrix(odesol)))
-			if(findSens){
-				odesol <- -odesol[,c("S", "nu_beta_S", "nu_gamma_S", "nu_N_S", "nu_I0_S")]
-				names(odesol) <- c("I", "nu_beta_I", "nu_gamma_I", "nu_N_I", "nu_I0_I")		
-			}else{
-				odesol <- -odesol
-				names(odesol) <- c("NA.t","I","NA.p")
-			}
-		}
-		
-		if(findSens){
-			return(odesol)
-		}else{
-			return(odesol[,"I"])
-		}
-		
-	})
+    with(as.list(params),{
+        if(incidence){
+            l <- length(t)
+            t.d <- diff(t[(l-1):l])
+            t <- c(t, t[l]+t.d)
+            t.interval <- diff(t)
+        }
+        
+        if(findSens){
+            func <- SIR.grad.sens
+            yini <- c(S = N*(1-i0), logI = log(N*i0),
+                      nu_beta_S = 0, nu_gamma_S = 0, nu_N_S = 1-i0,
+                      nu_I0_S = -N,
+                      nu_beta_I = 0, nu_gamma_I = 0, nu_N_I = i0,
+                      nu_I0_I = N)
+        }else{
+            func <- SIR.grad
+            yini <- c(S=N*(1-i0),logI=log(N*i0))
+        }
+        
+        odesol <- as.data.frame(ode(y=yini,
+                                    times=t,
+                                    func=func,
+                                    parms=params,
+                                    dllname = "fitsir",
+                                    initfunc = "initmod"))
+        
+        if(findSens){
+            sensName = c("nu_beta_S", "nu_gamma_S", "nu_N_S", "nu_I0_S", "nu_beta_I", "nu_gamma_I", "nu_N_I", "nu_I0_I")
+            
+            logSens <- c(beta, gamma, N, i0^2*exp(-qlogis(i0)))
+            
+            odesol[,sensName] = sweep(odesol[,sensName], 2, rep(logSens,2), "*")
+        }
+        
+        if(reportAll){
+            return(odesol)
+        }
+        
+        if(incidence){
+            odesol <- as.data.frame(diff(as.matrix(odesol)))
+            if(findSens){
+                odesol <- -odesol[,c("S", "nu_beta_S", "nu_gamma_S", "nu_N_S", "nu_I0_S")]
+                names(odesol) <- c("I", "nu_beta_I", "nu_gamma_I", "nu_N_I", "nu_I0_I")		
+            }else{
+                odesol <- -odesol
+                names(odesol) <- c("NA.t","I","NA.p")
+            }
+        }
+        
+        if(findSens){
+            return(odesol)
+        }else{
+            return(odesol[,"I"])
+        }
+        
+    })
 }
 
 ##' Normal log-likelihood for SIR trajectory
@@ -269,25 +293,25 @@ SIR.detsim <- function(t, params, findSens = FALSE,
 ##' @param dist conditional distribution of reported data (IGNORED)
 ##' @param debug print debugging output?
 SIR.logLik <- function(incidence = FALSE){
-	g <- function(params, count, tvec=NULL,
-    dist=dnorm2,
-    debug=FALSE) {
-		## HACK: nloptr appears to strip names from parameters
-		## if (is.null(params)) return(NA_real_) ## why ???
-		## if (is.null(names(params)) &&
-		##     !is.null(params)) ## ??? why ???
-		##     names(params) <- parnames(SIR.logLik)
-		if (is.null(tvec)) tvec <- seq(length(count))
-		if (debug) cat(params)
-		tpars <- trans.pars(params)
-		i.hat <- SIR.detsim(tvec,tpars, incidence = incidence)
-		
-		r <- -sum(dnorm2(count,i.hat,log=TRUE))
-		if (debug) cat(" ",r,"\n")
-		return(r)
-	}
-	attr(g, "parnames") <- c("log.beta","log.gamma","log.N","logit.i")
-	return(g)
+    g <- function(params, count, tvec=NULL,
+                  dist=dnorm2,
+                  debug=FALSE) {
+        ## HACK: nloptr appears to strip names from parameters
+        ## if (is.null(params)) return(NA_real_) ## why ???
+        ## if (is.null(names(params)) &&
+        ##     !is.null(params)) ## ??? why ???
+        ##     names(params) <- parnames(SIR.logLik)
+        if (is.null(tvec)) tvec <- seq(length(count))
+        if (debug) cat(params)
+        tpars <- trans.pars(params)
+        i.hat <- SIR.detsim(tvec,tpars, incidence = incidence)
+        
+        r <- -sum(dnorm2(count,i.hat,log=TRUE))
+        if (debug) cat(" ",r,"\n")
+        return(r)
+    }
+    attr(g, "parnames") <- c("log.beta","log.gamma","log.N","logit.i")
+    return(g)
 }
 ## parnames() specification required in order to use
 ## functions with parameters specified as a
@@ -318,17 +342,17 @@ SIR.logLik <- function(incidence = FALSE){
 ##' mean((1-ss/cc)^2)
 ##' mean((1-ss2/cc)^2)
 fitsir <- function(data,method="Nelder-Mead",
-  control=list(maxit=1e5),
-  timescale=NULL,
-  incidence = FALSE,
-  start=startfun(),debug=FALSE) {
+                   control=list(maxit=1e5),
+                   timescale=NULL,
+                   incidence = FALSE,
+                   start=startfun(),debug=FALSE) {
     g <- SIR.logLik(incidence = incidence)
-      mle2(g,
-        vecpar=TRUE,
-        start=start,
-        method=method,
-        control=control,
-        data=c(data,list(debug=debug)))
+    mle2(g,
+         vecpar=TRUE,
+         start=start,
+         method=method,
+         control=control,
+         data=c(data,list(debug=debug)))
 }
 
 ## Introducing sensitivity equations
@@ -356,51 +380,51 @@ findSens <- function(data, params, plot.it = FALSE, log = "xy", incidence = FALS
     }
     dSSQ <- 2 * (ssqL$pred - ssqL$obs)
     sensitivity <- with(ssqL$sim,
-         c(
-             SSQ = ssqL$SSQ,
-             SSQ_beta = sum(dSSQ * nu_beta_I),
-             SSQ_gamma = sum(dSSQ * nu_gamma_I),
-             SSQ_N = sum(dSSQ * nu_N_I),
-             SSQ_I0 = sum(dSSQ * nu_I0_I)
-         ))
+                        c(
+                            SSQ = ssqL$SSQ,
+                            SSQ_beta = sum(dSSQ * nu_beta_I),
+                            SSQ_gamma = sum(dSSQ * nu_gamma_I),
+                            SSQ_N = sum(dSSQ * nu_N_I),
+                            SSQ_I0 = sum(dSSQ * nu_I0_I)
+                        ))
     return(sensitivity)
 }
 
 fitsir.optim <- function(data,
-	start = startfun(),
-	incidence = FALSE,
-	verbose = FALSE,
-	plot.it = FALSE){
-	
-	if(plot.it){
-		plot(data)
-	}
-	
-	f.env <- new.env()
-	## set initial values
-	assign("oldSSQ",NULL,f.env)
-	assign("oldpar",NULL,f.env)
-	assign("oldgrad",NULL,f.env)
-	assign("data", data, f.env)
-	objfun <- function(par) {
-            if (identical(par,oldpar)) {
-                if (verbose) cat("returning old version of SSQ\n")
-                return(oldSSQ)
-            }
-            if (verbose) cat("computing new version (SSQ)\n")
-		
-            v <- findSens(data, par, incidence = incidence)
-            oldSSQ <<- v["SSQ"]
-            oldgrad <<- v[-1]
-            oldpar <<- par
-		
-            if(plot.it){
-                lines(data$tvec,
-                      SIR.detsim(data$tvec, trans.pars(par),
-                                 incidence = incidence))
-            }
+                         start = startfun(),
+                         incidence = FALSE,
+                         verbose = FALSE,
+                         plot.it = FALSE){
+    
+    if(plot.it){
+        plot(data)
+    }
+    
+    f.env <- new.env()
+    ## set initial values
+    assign("oldSSQ",NULL,f.env)
+    assign("oldpar",NULL,f.env)
+    assign("oldgrad",NULL,f.env)
+    assign("data", data, f.env)
+    objfun <- function(par) {
+        if (identical(par,oldpar)) {
+            if (verbose) cat("returning old version of SSQ\n")
             return(oldSSQ)
-	}
+        }
+        if (verbose) cat("computing new version (SSQ)\n")
+        
+        v <- findSens(data, par, incidence = incidence)
+        oldSSQ <<- v["SSQ"]
+        oldgrad <<- v[-1]
+        oldpar <<- par
+        
+        if(plot.it){
+            lines(data$tvec,
+                  SIR.detsim(data$tvec, trans.pars(par),
+                             incidence = incidence))
+        }
+        return(oldSSQ)
+    }
     environment(objfun) <- f.env
     gradfun <- function(par) {
         if (identical(par,oldpar)) {
@@ -415,11 +439,11 @@ fitsir.optim <- function(data,
         return(oldgrad)
     }
     environment(gradfun) <- f.env
-	
+    
     fit.p <- optim(fn = objfun,
                    par = start,
                    method = "L-BFGS-B",
                    gr = gradfun)$par
-	
+    
     return(fit.p)
 }
