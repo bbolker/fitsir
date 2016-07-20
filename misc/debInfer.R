d8466ef529afd39234383b6d90b76e04a9576ee4
parksw3 <parksw3@mcmaster.ca>
2016-07-19 22:21:31 -0400

    started working on Hessian

--- misc/debInfer.R
+++ misc/debInfer.R
@@ -26,34 +26,53 @@ SIR.logLik <- function(data, sim.data, samp){
 	return(r)
 }
 
+fitsir.deBInfer <- function(data,
+                            iter = 10000,
+                            burnin = 1000,
+                            prob = 0.95,
+                            start = NULL){
+  tvec = data$tvec
+  count = data$count
+  
+  is.null(start){
+    start = list(R0 = 2.3, r = 0.6, S0 = 10000, I0 = 4)
+  }
+  
+  with(as.list(start),{
+    ## It's actually R0-1
+    R0.1 <- debinfer_par(name = "R0.1", var.type = "de", fixed = FALSE,
+                         value = R0-1, prior="unif", hypers=list(min = 0, max = 30),
+                         prop.var=2, samp.type="rw")
+    
+    r <- debinfer_par(name = "r", var.type = "de", fixed = FALSE,
+                      value = 0.6, prior="unif", hypers=list(min = 0, max = 30),
+                      prop.var=0.3, samp.type="rw")
+    
+    S <- debinfer_par(name = "S", var.type = "init", fixed = FALSE,
+                      value = S0, prior="unif", hypers=list(min = 100, max = 100000),
+                      prop.var=10000, samp.type="rw")
+    
+    I <- debinfer_par(name = "I", var.type = "init", fixed = FALSE,
+                      value = I0, prior="unif", hypers=list(min = 0, max = 2*bombay2$count[1]),
+                      prop.var=2, samp.type="rw")
+    
+    R <- debinfer_par(name = "R", var.type = "init", fixed = TRUE,
+                      value = 0)
+    
+    mcmc.pars <- setup_debinfer(R0.1, r, I, S, R)
+    
+    iter = 10000
+    
+    mcmc_samples <- de_mcmc(N = iter, data=bombay2, de.model=SIR.grad,
+                            obs.model=SIR.logLik, all.params=mcmc.pars,
+                            Tmax = max(bombay2$tvec), data.times=bombay2$tvec, cnt=500,
+                            plot=TRUE, solver="ode")
+    
+  })
+  
+}
+
 library(deBInfer)
-R0.1 <- debinfer_par(name = "R0.1", var.type = "de", fixed = FALSE,
-									value = 2, prior="unif", hypers=list(min = 0, max = 30),
-									prop.var=1, samp.type="rw")
-
-r <- debinfer_par(name = "r", var.type = "de", fixed = FALSE,
-									value = 0.6, prior="unif", hypers=list(min = 0, max = 30),
-									prop.var=0.3, samp.type="rw")
-
-S <- debinfer_par(name = "S", var.type = "init", fixed = FALSE,
-									value = 10000, prior="unif", hypers=list(min = 100, max = 100000),
-									prop.var=1000, samp.type="rw")
-
-I <- debinfer_par(name = "I", var.type = "init", fixed = FALSE,
-									value = 4, prior="unif", hypers=list(min = 0, max = 2*bombay2$count[1]),
-									prop.var=2, samp.type="rw")
-
-R <- debinfer_par(name = "R", var.type = "init", fixed = TRUE,
-									value = 0)
-
-mcmc.pars <- setup_debinfer(R0.1, r, I, S, R)
-
-iter = 10000
-
-mcmc_samples <- de_mcmc(N = iter, data=bombay2, de.model=SIR.grad,
-												obs.model=SIR.logLik, all.params=mcmc.pars,
-												Tmax = max(bombay2$tvec), data.times=bombay2$tvec, cnt=500,
-												plot=TRUE, solver="ode")
 
 plot(mcmc_samples)
 
@@ -62,8 +81,10 @@ pairs(mcmc_samples, burnin = burnin, scatter=TRUE, trend=TRUE)
 
 post_traj <- post_sim(mcmc_samples, n=500, times=1:31, burnin=burnin, output = 'all', prob = 0.95)
 
-plot(post_traj, plot.type = "medianHDI", lty = c(2,1), lwd=3, col=c("red","grey20"),
+plot(post_traj, plot.type = "medianHDI", lty = c(1,2), lwd=3, col=c("red","grey20"),
      log = "y",
 		 panel.first=lines(bombay2, col="darkblue", lwd=2))
 
-
+plot(bombay2, log = "y")
+lines(post_traj[["median"]]$I)
+matlines(post_traj[["HDI"]]$I, col = 2, lty = c(2,2))
