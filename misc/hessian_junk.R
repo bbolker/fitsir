0c43f53f19ef4affdb0cbee20c56ec4b8428f018
parksw3 <parksw3@mcmaster.ca>
2016-07-27 20:29:30 -0400

    trying different things

--- misc/hessian_junk.R
+++ misc/hessian_junk.R
@@ -1,4 +1,13 @@
-g <- SIR.logLik(incidence = FALSE)
+library(devtools)
+load_all("..")
+
+bombay2 <- setNames(bombay, c("tvec", "count"))
+fpars <- coef(f1 <- fitsir(bombay2, start = startfun()))
+
+##more stable than fpars?
+fpars2 <- coef(f2 <- fitsir(bombay2, start = fpars))
+
+g <- SIR.logLik(incidence = FALSE, method = "rk4", hini = 0.1)
 tmpf <- function(log.beta,log.gamma,basepars=fpars,data=bombay2,
                  useSSQ=FALSE) {
     pars <- basepars
@@ -6,53 +15,44 @@ tmpf <- function(log.beta,log.gamma,basepars=fpars,data=bombay2,
     if (useSSQ) return(findSSQ(data,pars)$SSQ)
     return(g(pars,data$count))
 }
-summarize.pars(fpars)
-tmpf2 <- function(log.R0plus1,log.r,basepars=fpars,data=bombay2) {
-    pars <- basepars
-    pars[c("log.beta","log.gamma")] <- c(log.beta,log.gamma)
-    findSSQ(data,pars)$SSQ
-}
 
-tmpf(2.43,2.4)
-cc <- curve3d(tmpf(x,y),xlim=c(2.4,2.44),
-              ylim=c(2.37,2.42),n=c(41,41))
-ccssq <- curve3d(tmpf(x,y,useSSQ=TRUE),xlim=c(2.4,2.44),
-              ylim=c(2.37,2.42),n=c(41,41))
-cc2 <- curve3d(tmpf(x,y),xlim=c(2.42,2.435),
-              ylim=c(2.38,2.40),n=c(51,51))
-cc3 <- curve3d(tmpf(x,y,useSSQ=TRUE),xlim=c(2.42,2.435),
-              ylim=c(2.38,2.40),n=c(51,51))
+library(emdbook)
 
-cc$z[cc$z>1e3] <- NA
-cc$z2 <- cc$z
+fpars2
 
-which(cc$z>800,arr.ind=TRUE)
-cc$x[9]
-cc$y[3]
-points(cc$x[9],cc$y[3])
-tmpf(cc$x[9],cc$y[3])
-plot(count~tvec,data=bombay2)
-pp <- pars
-pp["log.beta"] <- cc$x[9]
-pp["log.gamma"] <- cc$y[3]
-ss <- SIR.detsim(bombay2$tvec,trans.pars(pp))
-lines(bombay2$tvec,ss)
+cc <- curve3d(tmpf(x,y, basepars = fpars2),xlim=c(2.518,2.52),
+              ylim=c(2.487,2.489),n=c(20,20))
 
-with(ccssq,image(x,y,log10(z-min(z,na.rm=TRUE)+100),
-              xlab="beta",ylab="gamma"))
-
-ss2 <- SIR.detsim(bombay2$tvec,trans.pars(pp),method="rk4",hini=0.001)
-
-with(ccssq,persp(x,y,log10(z-min(z)+100)))
-abline(a=0,b=1)
 library(rgl)
-with(ccssq,persp3d(x,y,log10(z-min(z,na.rm=TRUE)+100),col="gray"))
 open3d()
-with(cc,persp3d(x,y,log10(z-min(z,na.rm=TRUE)+10),col="blue"))
+with(cc,persp3d(x,y,log10(z-min(z,na.rm=TRUE)+0.01),col="blue"))
+
+min(cc$z)
+
+which(cc$z == min(cc$z), arr.ind = TRUE)
+
+m.x <- cc$x[19]
+d.x <- cc$x[19] - cc$x[17]
+
+m.y <- cc$y[19]
+d.y <- cc$y[19] - cc$y[17]
+
+cc.zoom <- curve3d(tmpf(x,y, basepars = fpars2),xlim=c(m.x - d.x, m.x + d.x),
+                   ylim=c(m.y-d.y,m.y+d.y),n=c(21,21))
+
 open3d()
-with(cc2,persp3d(x,y,log10(z-min(z,na.rm=TRUE)+0.0001),col="green"))
-open3d()
-with(cc3,persp3d(x,y,log10(z-min(z,na.rm=TRUE)+10),col="yellow"))
-with(cc,persp(x,y,z,ticktype="detailed",zlim=c(96000,1e6)))
+with(cc.zoom,persp3d(x,y,log10(z-min(z,na.rm=TRUE)+0.01),col="red"))
+
+with(cc.zoom,persp(x,y,log10(z-min(z)+100)))
+with(cc.zoom,image(x,y,log10(z-min(z,na.rm=TRUE)),
+               xlab="beta",ylab="gamma"))
+
+cc2 <- cc
+cc2$z[cc2$z > 167.9] <- NA
+
+with(cc2,image(x,y,log10(z-min(z,na.rm=TRUE)+1e-5),
+                   xlab="beta",ylab="gamma"))
 
 
+##very different....
+fpars3 <- coef(f1 <- fitsir(bombay2, start = fpars2))
