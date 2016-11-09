41a5a60a6c614a5837985d3747f086a109b5190f
parksw3 <parksw3@mcmaster.ca>
2016-11-09 14:23:12 -0500

    preliminary LHS results... (seems like fitting two parameters should work better)

---
+++ misc/LHSsim_pre.R
@@ -0,0 +1,81 @@
+library(fitsir)
+library(emdbook)
+library(rgl)
+source("stochsim_funs.R")
+
+truepars <- c(log.beta = log(2),
+    log.gamma = log(0.8),
+    log.N = log(1e6),
+    logit.i = qlogis(1e-5)
+)
+
+dd <- simfun(trans.pars(truepars), seed = 123, rpars = list(size = 4))
+tvec <- dd$tvec
+count <- dd$count
+
+lhsf <- function(start = truepars,
+                 range = 0.2,
+                 seed = NULL,
+                 n = 50){
+    if(!is.null(seed)) set.seed(seed)
+    
+    lhs.pars <- sapply(start, function(x){
+        xvec <- x * seq(1 - range, 1 + range, length.out = n)
+        sample(xvec)
+    })
+    return(lhs.pars)
+}
+
+lhspars <- lhsf(seed = 101)
+
+fn <- "LHSsim_pre.rda"
+
+## fitting... starting values are sampled using LHS
+
+if(file.exists(fn)){
+    load(fn)
+}elsle{
+    resList <- apply(lhspars, 1, function(x){
+        fitsir(dd, start = x)
+    })
+    save("lhspars", "resList", file = fn)
+}
+
+fitdf <- do.call("rbind", lapply(resList, function(x){
+    c(coef(x), ll = logLik(x))
+}))
+
+## it seems like most parameters converge to one place
+## one of them is getting stuck?
+pairs(fitdf)
+
+SIR.logLik <- fitsir:::SIR.logLik()
+
+## Assuming that we know N and gamma...
+tmpfun <- function(x, y, llpars = truepars){
+    llpars["log.beta"] <- x
+    llpars["log.gamma"] <- y
+    SIR.logLik(llpars, count, tvec)
+}
+
+cc <- curve3d(tmpfun(x, y),
+    xlim = truepars["log.beta"]*c(0.4, 1.6),
+    ylim = truepars["log.gamma"]*c(1.6, 0.4),
+    n = c(51, 51)
+)
+
+image(cc, xlab = "log.beta", ylab = "log.gamma")
+persp3d(cc, col = "blue")
+
+## might be better to use fitted parameters
+bestfit <- fitdf[which.max(fitdf[,5]),-5]
+
+cc2 <- curve3d(tmpfun(x, y, llpars = bestfit),
+              xlim = bestfit["log.beta"]*c(0.4, 1.6),
+              ylim = bestfit["log.gamma"]*c(1.6, 0.4),
+              n = c(51, 51)
+)
+
+image(cc2, xlab = "log.beta", ylab = "log.gamma")
+persp3d(cc2, col = "blue")
+
